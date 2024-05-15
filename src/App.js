a4f8baa5df2691934420a29ce8fd015b8779e6f7
victor@victor <victor.dmaina@gmail.com>
2024-05-15 19:39:45 +0300

    complete tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm Victor Maina</h1>
+      <img alt="My profile pic" src="https://via.placeholder.com/350" />
+      <h2>About Me</h2>
+      <p>
+        I am an aspiring Software Developer who loves interacting with various machines and bringing ideas to life. 
+      </p>
+      <div>
+        <a href="https://github.com/smigthereason">GitHub</a>
+        <a href="www.linkedin.com/in/victor-maina-389318301">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
